1dcc76d5d386aa7a41e34b849e58b1f1d59e3464
Mounika Reddy <150263001+moniieekaa@users.noreply.github.com>
2026-02-16 16:05:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         int a=10;
-        int b=20;
-        System.out.println("The sum of a and b is: " + (a + b));
+        int b=5;
+        System.out.println("The difference of a and b is: " + (a - b));
 
         System.out.println("Hello, World!");
     }
